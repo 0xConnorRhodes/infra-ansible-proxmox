832c637fe6528c9e69dfedb90acde5cc77c0972d
Connor Rhodes <connor@rhodes.contact>
2023-02-05 12:20:51 -0600

    added commands to switch to the fastest apt mirror

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
 # set proxmox llvm root to be all of the llvm volume
-# 1. delete the local lvm storage in the proxmox GUI
+# TODO 1. delete the local lvm storage in the proxmox GUI
 
 lvremove /dev/pve/data
 lvresize -l +100%FREE /dev/pve/root
 resize2fs /dev/mapper/pve-root
 
+apt install netselect-apt
 
+netselect-apt --arch amd64
+# TODO copy the url from the generated sources.list to the first two urls in /etc/apt/sources.list (but not the security updates repo
